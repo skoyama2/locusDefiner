570f11494712abefc43bf9d77d374afade1cfbdc
Satoshi Koyama <skoyama2@mgh.harvard.edu>
2026-03-31 21:16:10 -0400

    modify run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,12 +1,25 @@
 set -euo pipefail
 
+# Complie
+
 gcc -O2 -Wall -Wextra -o locusDefiner locusDefiner.c -lz -lm
 
-./locusDefiner \
+#  Downlaod Example File
+
+if [ ! -e ./example/QTL_BMI.EUR.gwama.sumstats.txt.gz ]; then
+
+  wget -O ./example/QTL_BMI.EUR.gwama.sumstats.txt.gz \
+    https://g-fce312.fd635.8443.data.globus.org/sumstats_downsized/EUR/QTL_BMI.EUR.gwama.sumstats.txt.gz
+
+fi
+
+# Execution
+
+time ./locusDefiner \
   --input ./example/QTL_BMI.EUR.gwama.sumstats.txt.gz \
   --output ./example_loci.tsv \
   --thr 5e-8 \
   --window 500000
 
 n=$(cat ./example_loci.tsv | sed '1d' | wc -l)
-echo ${n} loci detected
+echo ${n} loci detected # 707 loci
